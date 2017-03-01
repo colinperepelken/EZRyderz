29185d112a24d8c7f079c01476d2c0051d042267
Colin Bernard <colinjbernard@hotmail.com>
2017-02-28 18:16:24 -0800

    Laravel Framework created

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>EZRyderz</title>
+    </head>
+    <body>
+        <h1>Welcome to EZRyderz!</h1>
+        <p>Site coming soon!</p>
+    </body>
+</html>

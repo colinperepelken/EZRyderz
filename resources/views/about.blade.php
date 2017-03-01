db24d83540071fcbabd83090d0854e7076fe6d9c
colinbernard <colinjbernard@hotmail.com>
2017-03-01 10:58:09 -0800

    created simple about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>EZRyderz</title>
+    </head>
+    <body>
+        <h1>About Us</h1>
+        <p>This is our about section....</p>
+        <p>Created by bfkdsafdsajfjdsaf</p>
+    </body>
+</html>
+

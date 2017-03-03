709a5e27400a22183d4ce785e3a77e535dbe46b2
31137145 <31137145>
2017-03-02 16:56:28 -0800

    Images folder, prep to create home page

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -4,26 +4,25 @@
 
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>EZRyderz</title>
-    </head>
-    <body>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="<?php echo asset('css/all.css')?>" type="text/css"> 
+  </head>
+    
+  <body>
     <div class="container">
     <header class="row">
-      
+      <h1>Welcome to EZRyderz! Your simpe carpool solutionnnnnnn!</h1>
     </header>
     <div id="main" class="row">
-        <h1>Welcome to EZRyderz!</h1>
-        <p>Site coming soon!</p>
+      <p>Site coming soon!</p>
     </div>
     <footer class="row">
         <!-- TODO: add footer -->
     </footer>
     </div>
-    </body>
+  </body>
 </html>
 @endsection
